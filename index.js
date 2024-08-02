ede8a566615b73659fb8f489f48614e24c75d872
proxyerium <proxyerium@proton.me>
2024-08-03 06:00:17 +0800

    [new] light/dark theme for website demo
    - add button for dark/light theme
    - add some animations
    - add 2 assets

---
+++ index.js
@@ -0,0 +1,14 @@
+const root = document.documentElement;
+let darkMode = true
+
+const buttonTheme = document.querySelector("#theme");
+buttonTheme.addEventListener("click", ()=>{
+    if(darkMode){
+        root.style.setProperty("--bg-color", "#f5deb3");
+        root.style.setProperty("--txt-color", "#999999");
+    } else{
+        root.style.setProperty("--bg-color", "#3c3c3c");
+        root.style.setProperty("--txt-color", "#f0f8ff");
+    }
+    darkMode = !darkMode;
+});
