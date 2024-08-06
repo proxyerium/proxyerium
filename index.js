39ea06d1054ed54dabc991bf796d2e6256b07538
proxyerium <proxyerium@proton.me>
2024-08-06 23:57:57 +0800

    [update] visuals for website demo
    - improve visual effect of theme button
    - adjust animation of transition

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const root = document.documentElement;
+
+
+/*
+*  [appearance]
+*  toggle dark mode
+*/
 let darkMode = true
-
-const buttonTheme = document.querySelector('#button-theme');
-
-buttonTheme.addEventListener('click', ()=>{
+document.getElementById('button-theme').addEventListener('click', ()=>{
     if(darkMode){
+        root.style.setProperty('--icon-theme', 'url(assets/light.svg)');
         root.style.setProperty('--bg-color', '#f5deb3');
-        root.style.setProperty('--txt-color', '#999999');
+        root.style.setProperty('--txt-color', '#3e3e3e');
+        // root.style.setProperty('--bg-subcolor', '#f5deb3');
     } else{
+        root.style.setProperty('--icon-theme', 'url(assets/dark.svg)');
         root.style.setProperty('--bg-color', '#3c3c3c');
-        root.style.setProperty('--txt-color', '#f0f8ff');
+        root.style.setProperty('--txt-color', '#f3f3f3');
     }
     darkMode = !darkMode;
 });
