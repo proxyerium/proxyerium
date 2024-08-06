b1271c5a91848894e719a8fa3c5c5ba461d68dcb
proxyerium <proxyerium@proton.me>
2024-08-07 01:15:30 +0800

    [update] visuals for website demo
    - improve behavior of toggling theme
    - adjust theme color

--- index.js
+++ index.js
@@ -2,20 +2,11 @@ const root = document.documentElement;
 
 
 /*
-*  [appearance]
-*  toggle dark mode
+*  [theme]
+*  toggle dark/light theme
 */
-let darkMode = true
 document.getElementById('button-theme').addEventListener('click', ()=>{
-    if(darkMode){
-        root.style.setProperty('--icon-theme', 'url(assets/light.svg)');
-        root.style.setProperty('--bg-color', '#f5deb3');
-        root.style.setProperty('--txt-color', '#3e3e3e');
-        // root.style.setProperty('--bg-subcolor', '#f5deb3');
-    } else{
-        root.style.setProperty('--icon-theme', 'url(assets/dark.svg)');
-        root.style.setProperty('--bg-color', '#3c3c3c');
-        root.style.setProperty('--txt-color', '#f3f3f3');
-    }
-    darkMode = !darkMode;
+    const currentTheme = document.documentElement.getAttribute('data-theme');
+    const toggleTheme = (currentTheme === 'dark') ? 'light' : 'dark';
+    document.documentElement.setAttribute('data-theme', toggleTheme);
 });
